e9f13c8fd0b6fe5922ddf81c745ae37635479fe3
heoeuntaek <gjdmsxor@gmail.com>
2024-01-24 07:47:12 +0900

    backjoon

--- src/b1874.java
+++ src/b1874.java
@@ -11,45 +11,34 @@ public class b1874 {
 
         Stack<Integer> stack = new Stack<>();
 
+
         int n = Integer.parseInt(br.readLine());
         int x = 1;
         for (int i = 0; i < n; i++) {
             int number = Integer.parseInt(br.readLine());
             //number가 나와야 함
-            while (true) {
+            while (x <= number) {
+                stack.push(x);
+                sb.append("+").append('\n');
+                x++;
+            }
 
-                if (!stack.empty()) {
-                    if (stack.peek() > number) {
-                        sb.setLength(0);
-                        sb.append("NO").append('\n');
-                        bw.flush();
-                        bw.write(String.valueOf(sb));
-                        bw.close();
-                        br.close();
-                        return;
-                    }
+            if (!stack.empty()) {
+                if (stack.peek() > number) {
+                    sb.setLength(0);
+                    sb.append("NO").append('\n');
+                    break;
                 }
 
-                if (!stack.empty()) {
-                    if (stack.peek() == number) {
-                        stack.pop();
-                        sb.append("-").append('\n');
-                        break;
-                    }
-                }
-
-                if (x <= number) {
-                    stack.push(x);
-                    sb.append("+").append('\n');
-                    x++;
+                if (stack.peek() == number) {
+                    stack.pop();
+                    sb.append("-").append('\n');
                 }
 
             }
-
-
         }
+        bw.write(sb.toString());
         bw.flush();
-        bw.write(String.valueOf(sb));
         bw.close();
         br.close();
 

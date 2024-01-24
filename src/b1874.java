08daef1183c8874d0c5a04fc1660c54530631cd4
heoeuntaek <gjdmsxor@gmail.com>
2024-01-24 09:31:35 +0900

    backjoon

--- src/b1874.java
+++ src/b1874.java
@@ -13,6 +13,8 @@ public class b1874 {
 
 
         int n = Integer.parseInt(br.readLine());
+
+
         int x = 1;
         for (int i = 0; i < n; i++) {
             int number = Integer.parseInt(br.readLine());
